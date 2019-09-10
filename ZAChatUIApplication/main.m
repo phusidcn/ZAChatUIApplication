e578f29751b33e26634e908dd1fff4ce835f3c6a
Huynh Lam Phu Si <phusidcn@hotmail.com.vn>
2019-09-10 20:35:48 +0700

    Initial Commit

---
+++ ZAChatUIApplication/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZAChatUIApplication
+//
+//  Created by Huynh Lam Phu Si on 9/10/19.
+//  Copyright © 2019 Huynh Lam Phu Si. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
